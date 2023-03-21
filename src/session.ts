bd02a7bf0c56e90e238b71c8677ddbf08cec1744
Sean Schertell <sean@codepilot.com>
2023-03-21 11:25:51 -0700

    Fixed session.ts merge

--- src/session.ts
+++ src/session.ts
@@ -14,16 +14,8 @@ const redis = await connect({
 
 // pass redis connection into a new RedisStore. Optionally add a second string argument for a custom database prefix, default is 'session_'
 const store = new RedisStore(redis);
-let session;
-if (env.DEV) {
-  session = Session.initMiddleware(); // In-memory session for dev mode.
-} else {
-  const db = await getDb();
-  const store = new MongoStore(db, "zenjs_sessions");
-  session = Session.initMiddleware(store);
-}
 
 // Use in memory session store in dev mode, otherwise use MongoDB.
-export const session = env.DEV
+export const session = env.MODE === "dev"
   ? Session.initMiddleware()
   : Session.initMiddleware(store);

c990d465babc0809a3d5b61cfbb479cdf2c99b03
Sean Schertell <sean@codepilot.com>
2023-05-04 15:32:24 -0700

    Fixed z-active, z-disabled, z-selected bug

--- src/modifiers.js
+++ src/modifiers.js
@@ -10,88 +10,88 @@ export function parseModifiers(dom, ctx) {
   // First parse declarative modifiers.
 
   // z-active
-  const zActiveEls = dom.querySelectorAll('[z-active]');
+  const zActiveEls = dom.querySelectorAll("[z-active]");
   zActiveEls.forEach((el) => {
-    const zActiveValue = el.getAttribute('z-active');
+    const zActiveValue = el.getAttribute("z-active");
 
     // Special '@' value means the element is active current page matches href.
-    if (zActiveValue === '@') {
-      const href = el.getAttribute('href').split('#')[0].split('?')[0];
+    if (zActiveValue === "@") {
+      const href = el.getAttribute("href").split("#")[0].split("?")[0];
       const isActivePath = ctx.$meta.pathname.startsWith(href);
       if (isActivePath) {
-        el.classList.add('active');
-        el.setAttribute('z-active', 'true');
+        el.classList.add("active");
+        el.setAttribute("z-active", "true");
       } else {
-        el.classList.remove('active');
-        el.setAttribute('z-active', 'false');
+        el.classList.remove("active");
+        el.setAttribute("z-active", "false");
       }
     } // Otherwise, just add/remove the 'active' class.
     else {
       isFalsey(zActiveValue)
-        ? el.classList.remove('active')
-        : el.classList.add('active');
-      el.setAttribute('z-active', zActiveValue ? 'true' : 'false');
+        ? el.classList.remove("active")
+        : el.classList.add("active");
+      el.setAttribute("z-active", isFalsey(zActiveValue) ? "false" : "true");
     }
   });
 
   // z-invalid
-  const zInvalidEls = dom.querySelectorAll('[z-invalid]');
+  const zInvalidEls = dom.querySelectorAll("[z-invalid]");
   zInvalidEls.forEach((el) => {
-    const zInvalidValue = el.getAttribute('z-invalid');
+    const zInvalidValue = el.getAttribute("z-invalid");
     isFalsey(zInvalidValue)
-      ? el.classList.remove('invalid')
-      : el.classList.add('invalid');
-    el.setAttribute('z-invalid', zInvalidValue ? 'true' : 'false');
+      ? el.classList.remove("invalid")
+      : el.classList.add("invalid");
+    el.setAttribute("z-active", isFalsey(zActiveValue) ? "false" : "true");
   });
 
   // z-disabled
-  const zDisabledEls = dom.querySelectorAll('[z-disabled]');
+  const zDisabledEls = dom.querySelectorAll("[z-disabled]");
   zDisabledEls.forEach((el) => {
-    const zDisabledValue = el.getAttribute('z-disabled');
+    const zDisabledValue = el.getAttribute("z-disabled");
     isFalsey(zDisabledValue)
-      ? el.removeAttribute('disabled')
-      : el.setAttribute('disabled');
+      ? el.removeAttribute("disabled")
+      : el.setAttribute("disabled");
 
-    el.setAttribute('z-disabled', zDisabledValue ? 'true' : 'false');
+    el.setAttribute("z-disabled", isFalsey(zDisabledValue) ? "false" : "true");
   });
 
   // z-checked
-  const zCheckedEls = dom.querySelectorAll('[z-checked]');
+  const zCheckedEls = dom.querySelectorAll("[z-checked]");
   zCheckedEls.forEach((el) => {
-    const zCheckedValue = el.getAttribute('z-checked');
+    const zCheckedValue = el.getAttribute("z-checked");
     isFalsey(zCheckedValue)
-      ? el.removeAttribute('checked')
-      : el.setAttribute('checked', 'checked');
-    el.setAttribute('z-checked', zCheckedValue ? 'true' : 'false');
+      ? el.removeAttribute("checked")
+      : el.setAttribute("checked", "checked");
+    el.setAttribute("z-checked", isFalsey(zCheckedValue) ? "false" : "true");
   });
 
   // z-selected
-  const zSelectedEls = dom.querySelectorAll('[z-selected]');
+  const zSelectedEls = dom.querySelectorAll("[z-selected]");
   zSelectedEls.forEach((el) => {
-    const zSelectedValue = el.getAttribute('z-selected');
+    const zSelectedValue = el.getAttribute("z-selected");
     isFalsey(zSelectedValue)
-      ? el.removeAttribute('selected')
-      : el.setAttribute('selected', 'selected');
-    el.setAttribute('z-selected', zSelectedValue ? 'true' : 'false');
+      ? el.removeAttribute("selected")
+      : el.setAttribute("selected", "selected");
+    el.setAttribute("z-selected", isFalsey(zSelectedValue) ? "false" : "true");
   });
 
   // Then parse imperative modifiers.
   for (const [id, modifiers] of Object.entries(ctx.elementsToModify)) {
     modifiers.forEach((modifier) => {
       switch (modifier.type) {
-        case 'addClass':
+        case "addClass":
           dom.getElementById(id)?.classList.add(...modifier.data);
           break;
-        case 'removeClass':
+        case "removeClass":
           dom.getElementById(id)?.classList.remove(...modifier.data);
           break;
-        case 'setAttr':
+        case "setAttr":
           dom
             .getElementById(id)
             ?.setAttribute(modifier.data[0], modifier.data[1]);
 
           break;
-        case 'removeAttr':
+        case "removeAttr":
           dom.getElementById(id)?.removeAttribute(modifier.data[0]);
           break;
       }
@@ -101,5 +101,5 @@ export function parseModifiers(dom, ctx) {
 }
 
 function isFalsey(value) {
-  return ['false', '0', 'null', 'undefined', ''].includes(value);
+  return ["false", "0", "null", "undefined", ""].includes(value);
 }
